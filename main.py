2e24bd7265ba0d250f1e8e3901fa8d45c77c266a
ZC-A <1483681501@qq.com>
2023-10-11 20:08:20 +0800

    add query structure

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 from loguru import logger
 from config import conf
 from graph.client import client
-from redisgraph import Node, Edge, Graph, Path
+from redisgraph import Graph
 
 
 if __name__ == '__main__':
-    logger.info('启动redis graph....')
-    logger.info('装载配置文件')
+    logger.info('init redis graph....')
+    logger.info('load config')
     if not conf:
-        logger.error('装载失败')
+        logger.error('load failure')
         exit(1)
     client = client(conf=conf)
     if not client:
         exit(1)
-    redis_graph = Graph('social', client)
-    query = """MATCH (p:person) RETURN p.name"""
+    redis_graph = Graph('SocialGraph', client)
+    with open('queries/1.cypher', 'r') as f:
+        query = f.read()
     results = redis_graph.query(query)
-    for result in results.result_set:
-        logger.info(str(result))
---
+++ queries/Person_isLocatedIn_City.cypher
@@ -0,0 +1,11 @@
+MATCH (n:Person {id: '{}'})-[:IS_LOCATED_IN]->(p:City)
+RETURN
+    n.id AS personid,
+    n.firstName AS firstName,
+    n.lastName AS lastName,
+    n.birthday AS birthday,
+    n.locationIP AS locationIP,
+    n.browserUsed AS browserUsed,
+    p.id AS cityId,
+    n.gender AS gender,
+    n.creationDate AS creationDate
